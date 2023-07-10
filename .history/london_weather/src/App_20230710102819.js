d1e576df1c9e6183a336e172f5a825edae6127ce
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-07-10 10:31:40 +0100

    bugs fixed

--- .history/london_weather/src/App_20230710102819.js
+++ .history/london_weather/src/App_20230710102819.js
@@ -7,7 +7,7 @@ import "./App.css";
 
 function App() {
     const [currentWeather, setCurrentWeather] = useState(null);
-    const [forecast, setForecast] = useState(null);
+    const [forecast, setForecast] = useState(0);
 
     const handleOnSearchChange = (searchData) => {
         const [lat, lon] = searchData.value.split(" ");
